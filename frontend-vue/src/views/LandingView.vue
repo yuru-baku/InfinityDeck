a2ed21efd997847eb83ade8ffddae6d32ae2bf09
Frederick Behringer <f.-behringer@t-online.de>
2023-12-29 14:11:41 +0100

    started migration to Vue!

---
+++ frontend-vue/src/views/LandingView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  let name = ref('');
+  let socket: WebSocket;
+
+  function joinRoom() {
+      socket = new WebSocket(`ws://localhost:8080?name=${name}`);
+
+      socket.addEventListener('message', (event) => {
+          console.log(event)
+      });
+  }
+</script>
+
+<template>
+  <main class="vertical">
+    <form class="frame">
+        <label for="nickname">Choose your name</label>
+        <input type="text" id="nickname" default="LustigerName420" v-model="name">
+        <button type="button" class="startButton" @click="joinRoom()">Start</button>
+    </form>
+  </main>
+</template>
+
+<style lang="scss">
+  @import "@/styles/style.sass";
+</style>
---
+++ frontend-vue/src/views/LobbyCreaterView.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <main class="vertical">
+        <div id="playerList" class="frame">
+            <div class="playerStatus">
+                <div class="playerOnline"></div>Player420
+            </div>
+            <div class="playerStatus">
+                <div class="playerOffline"></div>empty
+            </div>
+            <div class="playerStatus">
+                <div class="playerOffline"></div>empty
+            </div>
+            <div class="playerStatus">
+                <div class="playerOffline"></div>empty
+            </div>
+            <button id="invite">Copy Invite</button>
+        </div>
+        <div id="playerList" class="frame">
+            <div id="chooseGame">
+                <p>ROMME</p>
+                <p>UNO</p>
+            </div>
+            <div></div>
+        </div>
+        <button>Start</button>
+    </main>
+</template>
