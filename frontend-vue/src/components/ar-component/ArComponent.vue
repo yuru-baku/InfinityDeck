0a6ff7eb446f33d919e7e322eb50d1639febdb27
Tom <tom@gnaudschun.net>
2024-03-19 15:45:03 +0100

    version with working click

--- frontend-vue/src/components/ar-component/ArComponent.vue
+++ frontend-vue/src/components/ar-component/ArComponent.vue
@@ -293,7 +293,7 @@ onUnmounted(() => {
             arjs="sourceType: webcam; detectionMode: mono_and_matrix; matrixCodeType: 5x5_BCH_22_7_7; debugUIEnabled: false; sourceWidth:1920; sourceHeight:1080;"
             cursor="rayOrigin: mouse"
         >
-            <a-entity camera id="userCamera"> </a-entity>
+            <a-camera camera id="userCamera"> </a-entity>
         </a-scene>
     </div>
 </template>
